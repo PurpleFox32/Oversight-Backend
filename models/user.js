981dcf88b9d85c3c340baeeab8a28d094d7e49f5
Abe <abealto9@gmail.com>
2022-06-03 14:51:12 -0400

    Adding project files

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      this.hasMany(models.Post, {
+        foreignKey: 'id',
+      });
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  return User;
+};
---
+++ routes/users.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+const { User } = require('../models');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  User.findAll().then((userList) => {
+    res.json(userList);
+  });
+});
+
+/* GET /:id get individual user */
+router.get('/:id', (req, res, next) => {
+  const userId = parseInt(req.params.id);
+
+  User.findOne({
+    where: {
+      id: userId,
+    },
+  }).then(
+    (theUser) => {
+      if (theUser) {
+        res.json(theUser);
+      } else {
+        res.status(404).send();
+      }
+    },
+    (err) => {
+      res.status(500).send(err);
+    }
+  );
+});
+
+/* POST create a user */
+router.post('/', (req, res, next) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((newUser) => {
+      res.json(newUser);
+    })
+    .catch(() => {
+      res.status(400).send();
+    });
+});
+
+/* PUT update a user */
+router.put('/:id', (req, res, next) => {
+  const userId = parseInt(req.params.id);
+
+  if (!catId || catId <= 0) {
+    res.status(400).send('Invalid ID');
+    return;
+  }
+
+  User.update(
+    {
+      email: req.body.email,
+    },
+    {
+      where: {
+        id: userId,
+      },
+    }
+  )
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(() => {
+      res.status(400).send();
+    });
+});
+
+/* DELETE delete a user */
+
+module.exports = router;
